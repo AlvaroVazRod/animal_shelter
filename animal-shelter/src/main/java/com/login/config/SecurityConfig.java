5011c8cc859ae7a0a67c9dc3c6a7f35fa3c36942
Alvaro Vazquez <alvarovazrod@gmail.com>
2025-05-13 19:55:12 +0200

    Configuracion de paths

--- animal-shelter/src/main/java/com/login/config/SecurityConfig.java
+++ animal-shelter/src/main/java/com/login/config/SecurityConfig.java
@@ -1,6 +1,5 @@
 package com.login.config;
 
-import com.login.service.impl.CustomUserDetailsService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -8,6 +7,8 @@ import org.springframework.security.config.annotation.authentication.configurati
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 public class SecurityConfig {
@@ -19,19 +20,14 @@ public class SecurityConfig {
             .authorizeHttpRequests(auth -> auth
                 .requestMatchers(
                     "/api/**",
+                    "/webhook",
                     "/swagger-ui.html",
                     "/swagger-ui/**",
                     "/v3/api-docs/**"
                 ).permitAll()
-                .anyRequest().authenticated()
+                .anyRequest().permitAll()
             )
-            .formLogin(form -> form
-                .loginPage("/login")
-                .defaultSuccessUrl("/dashboard", true)
-                .failureUrl("/login?error=true")
-                .permitAll()
-            )
-            .logout(logout -> logout.permitAll());
+            .httpBasic(withDefaults()); // No redirecciones
 
         return http.build();
     }
