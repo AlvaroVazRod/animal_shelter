8968f1e99fabea495172526ac6df5cfe7fc2e805
Alvaro Vazquez <alvarovazrod@gmail.com>
2025-05-23 18:05:22 +0200

    Imagenes

--- animal-shelter/src/main/java/com/login/config/WebConfig.java
+++ animal-shelter/src/main/java/com/login/config/WebConfig.java
@@ -1,7 +1,5 @@
 package com.login.config;
 
-import java.io.File;
-
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -15,7 +13,6 @@ public class WebConfig implements WebMvcConfigurer {
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/images/animal/**")
                 .addResourceLocations("file:///C:/Users/ALVARO/git/repository/animal-shelter/uploads/animals/");
-        System.out.println("Ruta absoluta: " + new File("uploads/animals/luna.jpg").getAbsolutePath());
     }
 
 }
