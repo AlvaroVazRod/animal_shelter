596f5da1b357540d024edf44b86546a8a76aac7a
Alvaro Vazquez <alvarovazrod@gmail.com>
2025-05-28 16:53:57 +0200

    CarruselAnimalImage

--- animal-shelter/src/main/java/com/login/repository/AnimalRepository.java
+++ animal-shelter/src/main/java/com/login/repository/AnimalRepository.java
@@ -4,10 +4,20 @@ import com.login.model.Animal;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface AnimalRepository extends JpaRepository<Animal, Long> {
-    Page<Animal> findBySpecies(String species, Pageable pageable);
-    Page<Animal> findByGender(boolean gender, Pageable pageable);
+    @EntityGraph(attributePaths = "images")
     Page<Animal> findBySpeciesAndGender(String species, boolean gender, Pageable pageable);
+
+    @EntityGraph(attributePaths = "images")
+    Page<Animal> findBySpecies(String species, Pageable pageable);
+
+    @EntityGraph(attributePaths = "images")
+    Page<Animal> findByGender(boolean gender, Pageable pageable);
+
+    @EntityGraph(attributePaths = "images")
+    Page<Animal> findAll(Pageable pageable);
 }
