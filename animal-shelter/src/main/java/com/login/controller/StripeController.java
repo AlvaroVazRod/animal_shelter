054d02b62754ca9b4c8e0f6fb6516bacf13b2f76
AlvaroVazRod <alvarovazrod@gmail.com>
2025-06-04 14:14:28 +0200

    niniedades 2

--- animal-shelter/src/main/java/com/login/controller/StripeController.java
+++ animal-shelter/src/main/java/com/login/controller/StripeController.java
@@ -60,7 +60,7 @@ public class StripeController {
         ProductAndPrice result = stripeService.ensureActiveProductAndPrice(
                 animal.getStripeProductId(),
                 animal.getStripePriceId(),
-                "Apadrina " + animal.getName(),
+                "Apadrinar a " + animal.getName(),
                 animal.getDescription(),
                 animal.getSponsorPrice()
         );
